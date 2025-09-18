ac9a9b24be1ee1d74bd2e3430d3b0601b903fca3
ToriLutz <vglutz@comcast.net>
2025-09-18 10:04:35 -0700

    error

--- server.js
+++ server.js
@@ -49,3 +49,22 @@ app.listen(port, () => {
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
