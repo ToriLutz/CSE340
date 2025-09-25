61c58d34f045e46afd265ac590990d95463d1ebb
ToriLutz <vglutz@comcast.net>
2025-09-25 14:13:57 -0700

    stuff

---
+++ routes/accountRoute.js
@@ -0,0 +1,32 @@
+// Needed Resources 
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require('../utilities');
+router.get('/detail/:account_id', accountController.buildLogin);
+router.get('/login', accountController.buildLogin);
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/register", accountController.buildRegister);
+router.get("/login", utilities.handleErrors(accountController.buildRegister));
+
+
+
+// Route to build account by classification view
+router.get("/type/:account_id", accountController.buildLogin);
+router.get("/type/:account_id", accountController.buildRegister);
+
+
+
+/* *****************************
+*   Register new account
+* *************************** */
+async function registerAccount(account_firstname, account_lastname, account_email, account_password){
+  try {
+    const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *"
+    return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
+  } catch (error) {
+    return error.message
+  }
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,11 +13,40 @@ const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
 const inventoryRoute = require('./routes/inventoryRoute');
+const accountRoute = require("./routes/accountRoute");
 const errorTestRoutes = require('./routes/errorTest');
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 
 
 
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * view Engine and Templates
@@ -29,8 +58,10 @@ app.use(express.static('public'))
 
 
 
+
 // Use the route
 app.use('/inv', inventoryRoute);
+app.use('/account', require("./routes/accountRoute"));
 
 
 
