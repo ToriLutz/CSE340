2d71c33fbf0812fa143e405717d8cc941759306e
ToriLutz <vglutz@comcast.net>
2025-09-29 10:14:19 -0700

    stuff

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -7,12 +7,16 @@ router.get('/detail/:inv_id', invController.detailVehicle);
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildByInventoryId));
+
 
 
 // router.get('/test', (req, res) => {
 //   res.send('Inventory route is working!');
 // });
 
+
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
 
 module.exports = router;
 
